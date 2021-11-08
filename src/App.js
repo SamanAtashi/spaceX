cd2b8347f00cf110027d0bfb45c1f79177fc04a8
SamanAtashi <milady00126@gmail.com>
2021-11-08 15:57:31 +0300

    Add linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navbar from './Components/Navbar';
 
 function App() {
-	return (
-		<div>
-			<Navbar />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
