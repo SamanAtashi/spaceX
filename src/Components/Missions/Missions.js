230c16b824302a120df33bd1277035db75101db3
SamanAtashi <milady00126@gmail.com>
2021-11-09 17:38:52 +0300

    Add Tables

--- src/Components/Missions/Missions.js
+++ src/Components/Missions/Missions.js
@@ -1,27 +1,38 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 // import store from '../../redux/configureStore';
+import { Table } from 'react-bootstrap';
 import { fetchMissions } from '../../redux/missions/missions';
 
 const Missions = () => {
   const dispatch = useDispatch();
-  const sth = useSelector((state) => state);
+  const sth = useSelector((state) => state.missions);
 
   useEffect(() => {
     dispatch(fetchMissions());
   }, []);
 
-  const dosth = () => sth.missions.map((item) => (
-    <p key={item.mission_id}>{item.mission_name}</p>
+  const dosth = () => sth.map((item) => (
+    <tr key={item.mission_id}>
+      <td>{item.mission_name}</td>
+      <td>{item.description}</td>
+      <td>Otto</td>
+    </tr>
   ));
 
   return (
     <div>
-      <p>this is Missions</p>
-      {dosth()}
-      {/* {missions.map((item) => (
-        <p key="item.mission_id">{item}</p>
-      ))} */}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Missions</th>
+            <th>Desciptions</th>
+            <th>Statue</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>{dosth()}</tbody>
+      </Table>
     </div>
   );
 };
