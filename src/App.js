cdfade54243124e77af49c8caf8ad2fd15e2d781
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-11-12 11:54:08 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Components/Home';
-import Missions from './Components/Missions/Missions';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Rockets from './Components/Rockets/Rockets';
+import Missions from './Components/Missions/Missions';
 
+const routes = [
+  {
+    name: 'Rockets',
+    path: '/',
+    component: <Rockets />,
+  },
+  {
+    name: 'Missions',
+    path: '/missions',
+    component: <Missions />,
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="Missions" element={<Missions />} />
-        <Route path="Rockets" element={<Rockets />} />
-      </Routes>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="app">
+        <Navbar title="Space Travelers' Hub" routes={routes} />
+        <Routes>
+          { routes.map(
+            ({ path, component }) => (
+              <Route key={path} path={path} element={component} />
+            ),
+          )}
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import store from './redux/configStore';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
